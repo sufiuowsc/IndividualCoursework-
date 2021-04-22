28c305cdaf54650f9b7fb1e6c3bd22b63b57ed45
sufiuowsc <82746739+sufiuowsc@users.noreply.github.com>
2021-04-22 14:41:18 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 }
 
-provider "random" {}
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
 
-resource "random_pet" "table_name" {}
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
+  tags = {
+    Name = var.instance_name
   }
 }
