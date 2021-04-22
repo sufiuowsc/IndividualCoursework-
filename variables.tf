28b28d15c4dba85574ef787a810f0c9fda29cc53
sufiuowsc <82746739+sufiuowsc@users.noreply.github.com>
2021-04-22 14:42:15 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,5 @@
-variable "aws_region" {
-  type    = string
-  default = "us-west-1"
-}
-
-variable "db_table_name" {
-  type    = string
-  default = "terraform-learn"
-}
-
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 1
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
 }
